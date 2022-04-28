b57bdac9ff797356ce3edf1ee7ef17694e154410
Bruno Carvalho <brunocarvalho9810@gmail.com>
2022-04-28 20:56:34 -0300

    Primeiro commit de criacao do projeto

---
+++ src/main/java/com/uploadarquivosapirest/UploadArquivosApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.uploadarquivosapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadArquivosApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadArquivosApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uploadarquivosapirest/UploadArquivosApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uploadarquivosapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadArquivosApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
